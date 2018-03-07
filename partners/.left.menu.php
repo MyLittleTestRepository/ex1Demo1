a0ccb59e9bf90852bb9e4b04ccda89d5e66f7433
Averin Dmitry <averin@intaro.email>
2018-03-07 11:39:42 +0300

    ex1-1

---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"./index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"./howtosignup.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/events/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partners/index.php
@@ -0,0 +1,3 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Условия сотрудничества");
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
